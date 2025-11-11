60f42f07bab4ae56edff586edca14598e4c6b908
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-11-11 16:10:56 -0300

    cadastrando o routes da mensagem e enviando a razão social para o banco

--- src/controllers/mensagemController.js
+++ src/controllers/mensagemController.js
@@ -1,18 +1,21 @@
 var mensagemModel = require("../models/mensagemModel");
 
-function enviar(){
+function enviar(req, res){
     var email = req.body.emailServer;
     var pais = req.body.paisServer;
     var mensagem = req.body.mensagemServer;
+    var razaoSocial = req.body.razaoSocialServer;
 
     if(email == ''){
         res.status(400).send("Email não inserido corretamente");
     }else if(pais == ''){
         res.status(400).send("O campo 'pais' não foi inserido");
+    }else if(razaoSocial == ''){
+        res.status(400).send("O campo 'razaoSocial' não foi inserido");
     }else if(mensagem == ''){
         res.status(400).send("A mensagem não foi inserida");
     }else{
-        mensagemModel.enviar(email, pais, mensagem)
+        mensagemModel.enviar(email, pais, mensagem, razaoSocial)
         .then(
             function (resultado){
                 res.json(resultado);
