a9f2f6904879d62b2539cb5e062938e8ffbe7141
Matheus-valle <matheus.valle@sptech.school>
2025-12-02 13:32:54 -0300

    voltando fetch listarSafras()

--- src/models/safraModel.js
+++ src/models/safraModel.js
@@ -1,31 +1,20 @@
 var database = require("../database/config")
 
 
-function mostrarSafra(email, id_usuario) {
+function mostrarSafra(id_usuario) {
 
     var instrucaoSql  = `
-    SELECT 
-    s.idSafra,
-    sen.idsensor,
-    sen.modelo,
-    sen.status_ativo,         
-    v.valor_temperatura,
-    v.valor_umidade,
-    v.situacao_temperatura,
-    v.situacao_umidade,
-    v.situacao_safra
-    FROM safra_wasabi s
-JOIN responsavel r
-    ON r.fk_safra = s.idSafra
-JOIN funcionario f
-    ON r.fk_funcionario = f.idFuncionario
-JOIN sensor sen
-    ON sen.fk_safra = s.idSafra
-JOIN vw_situacao_safra v
-    ON v.idSafra = s.idSafra
-       AND v.idsensor = sen.idsensor
-WHERE f.email = '${email};' 
-    AND f.idFuncionario = '${id_usuario}';` 
+           SELECT *
+        FROM vw_situacao_safra v
+        JOIN responsavel r ON v.idSafra = r.fk_safra
+        WHERE id_registro = (
+            SELECT MAX(id_registro)
+            FROM vw_situacao_safra
+            WHERE idsensor = v.idsensor
+        )
+        AND fk_funcionario = '${id_usuario}'
+        ORDER BY idSafra ASC
+        ;` 
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
--- src/routes/safra.js
+++ src/routes/safra.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var safraController = require("../controllers/safraConstroller");
 
 
-router.get("/mostrarSafra/:email/:id_usuario", function (req, res) {
+router.get("/mostrarSafra/:id_usuario", function (req, res) {
     safraController.mostrarSafra(req, res);
 })
 
